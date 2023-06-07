000c1d4323ae9bcc77662daef10251a13c5e5b9b
hanlee0519-ko <ahsj22@naver.com>
2023-06-07 13:37:11 +0900

    Testing Deep Copy for Nasted Object with Recursive Function

--- test/fn.js
+++ test/fn.js
@@ -1,5 +1,13 @@
 function deepCopy(obj) {
-  return { ...obj };
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
+  }
+
+  let copy = {};
+  for (let key in obj) {
+    copy[key] = deepCopy(obj[key]);
+  }
+  return copy;
 }
 
 module.exports = deepCopy;
